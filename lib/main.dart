5d13bb076cb98af80abc6c0fbc8906ac634d399c
utkarsh <utkarsh@debian.debian.com>
2023-10-20 13:48:25 +0530

    fixed bug: not initialized error

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool hasSeenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
   runApp(ProviderScope(child: MyApp(hasSeenOnboarding: hasSeenOnboarding)));
